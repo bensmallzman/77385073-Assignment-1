043204e1b80e41e12ceb2710987923177bd470b9
Ben Smallzman <bensmallzman@gmail.com>
2021-09-19 15:12:05 -0400

    commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// Ben Smallzman
+
+// Questions 1 and 2
+let myNumber = 5;
+
+// Question 3
+let myOtherNumber = 5;
+
+// Question 4
+let myTotal = myNumber * myOtherNumber;
+
+// Question 5
+let myName = "";
+let myCombo = (myNumber + myOtherNumber).toString();
+
+// Question 6
+let myOtherCombo = myNumber + myOtherNumber;
+
+// Question 7
+let myRemainder = myNumber % 5;
+
+// Question 8
+let i = 2;
+
+// Question 9
+let int = 4;
+
+// Question 10
+let myBoolean = false;
